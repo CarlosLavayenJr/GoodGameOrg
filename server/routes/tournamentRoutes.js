2862d345204fd0569f9823fb2f8200c7f5e31286
CARLOSSPC\Carlos <clavayen98@hotmail.com>
2024-06-12 13:16:21 -0400

    fixed file structure

--- server/routes/tournamentRoutes.js
+++ server/routes/tournamentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { createTournament, getTournaments, updateTournament, deleteTournament } = require('../controllers/tournamentController');
-const authMiddleware = require('../middleware/authMiddleware');
+const { createTournament, getTournaments, updateTournament, deleteTournament } = require('../../controllers/tournamentController');
+const authMiddleware = require('../../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/tournaments', authMiddleware, createTournament);
