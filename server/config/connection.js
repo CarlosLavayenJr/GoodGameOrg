8c49a98c9b9394e3cd690f7509d9c43d9bd3f8e0
Micah Read <micahreadmgmt@gmail.com>
2024-06-17 18:16:14 -0400

    Revert "connection.js for render deployment"

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/goodgame', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Successfully connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB', error);
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/goodgame');
 
 module.exports = mongoose.connection;
